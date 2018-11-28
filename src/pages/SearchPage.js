1efd8c0038659366c047585df1d3e2ef6953a9b9
Shin Changseon <skywing27@gmail.com>
2018-11-28 12:08:29 +0900

    feat: add Router Component & Lib: add react-router-dom module default App Component changed to Router Component added react-router-dom module to use react-router feature

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>여기는 검색결과 페이지</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
